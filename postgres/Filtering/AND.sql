43ca995b32ed1e9a68295c8f2af42d56b1ac3cac
Kgotlelelo Masenamela <kgotlelelo.masenamela1@vcontractor.co.za>
2024-04-07 09:33:47 +0200

    where and or

---
+++ postgres/Filtering/AND.sql
@@ -0,0 +1,23 @@
+-- SELECT 1 = 1 AS result;
+
+-- SELECT true AND true AS result;
+
+-- SELECT true AND false AS results;
+
+-- SELECT true AND null AS result;
+
+-- SELECT false AND false AS result;
+
+-- SELECT false AND null AS result;
+
+-- SELECT null AND null AS result
+
+-- SELECT
+-- 	title,
+-- 	length,
+-- 	rental_rate
+-- FROM
+-- 	film
+-- 	WHERE
+-- 		length > 180
+-- 		AND rental_rate < 1;
---
+++ postgres/Filtering/OR.sql
@@ -0,0 +1,17 @@
+-- SELECT true OR true AS result;
+-- SELECT true OR false AS result;
+-- SELECT true OR null AS result;
+-- SELECT false OR false AS result;
+-- SELECT false OR null AS result;
+-- SELECT false OR false AS result;
+-- SELECT null OR null AS result;
+
+-- SELECT
+-- 	title,
+-- 	rental_rate
+-- FROM
+-- 	film
+-- WHERE
+--  	rental_rate = 0.99 OR
+-- 	rental_rate = 2.99;
+
